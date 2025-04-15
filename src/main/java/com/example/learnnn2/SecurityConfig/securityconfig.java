c3724c71c8f2f77abe66c6ed048f18ce3ee77b4b
sheldon34 <mubokasheldonmuboka@gmail.com>
2025-04-16 00:22:28 +0300

    1st_unitTesting

--- src/main/java/com/example/learnnn2/SecurityConfig/securityconfig.java
+++ src/main/java/com/example/learnnn2/SecurityConfig/securityconfig.java
@@ -2,7 +2,7 @@ package com.example.learnnn2.SecurityConfig;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @Configuration
 
